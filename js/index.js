cacd66fbcb4c934e05bb2e7f7351c8fda7505839
jooyas <sayoojsayu428@gmail.com>
2024-05-22 23:38:00 +0530

    major changes removed unwanted variable declarations and reduced javascript created news full view pages

--- js/index.js
+++ js/index.js
@@ -1,48 +1,44 @@
 const newscards = document.getElementById("articles");
 const newsapi = "https://newsapi.org/v2/everything?q=technology&sortBy=popularity&apiKey=f913415ff5af44b5bc417d2bb5e6b7de";
-fetch(newsapi)
+
+fetch(newsapi) 
 .then(result => result.json())
 .then(data => {
-    const cardTemplate = `
-            <article class="card">
-                <div class="img-container">
-                    <img src="" class="news-image" alt="">
-                </div>
-                <div class="card-content">
-                    <span class="news-source"></span>
-                    <h3 class="news-heading"></h3>
-                    <p class="news-description"></p>
-                </div>
-            </article>
-        `;
 
+    data.articles.forEach(article => {
 
-        data.articles.forEach(article => {
+        //news articles with no title and image will be rejected
         if(article.title && article.urlToImage) {
-            // Clone the card template for each article
+
+            //created a template for newscard
+            //without declaring data from api goes directly into html
+            const cardTemplate = `
+                <article class="card" >
+                    <div class="img-container">
+                        <img src="${article.urlToImage}" class="news-image" alt="an image potraits the content of article">
+                    </div>
+                    <div class="card-content">
+                        <span class="news-source">${article.source.name}</span>
+                        <h3 class="news-heading">${article.title}</h3>
+                        <p class="news-description">${article.description}</p>
+                    </div>
+                </article>
+            `;
+            
+            // pasting the card template for every article
             const newCard = document.createElement('div');
             newCard.innerHTML = cardTemplate.trim();
             const card = newCard.firstChild;
-        
-            // add data to each card
-            const sourceName = card.querySelector('.news-source');
-            const newsHeader = card.querySelector('.news-heading');
-            const newsImage = card.querySelector('.news-image');
-            const newsDescription = card.querySelector('.news-description');
-            //const newsLink = card.querySelector('.news-link');
-            
-            sourceName.textContent = article.source.name;
-            newsHeader.textContent = article.title;
-            newsDescription.textContent = article.description;
-            newsImage.src = article.urlToImage;
-            //newsLink.href = article.url;
-                    
-            newsImage.addEventListener('error', function() {
-                newsImage.src = "./images/placeholder-img.jpg"
-            });
-            
+           
+
             // Append the card to the news cards section
-            newscards.appendChild(card);
+            newscards.appendChild(card); 
+            
+             //if imageurl not getting image pastes placeholder image
+            const newsImage = card.querySelector(".news-image");
+            newsImage.onerror = function() {
+                newsImage.src = "./images/placeholder-img.jpg"
+            };
         }
     });
             
